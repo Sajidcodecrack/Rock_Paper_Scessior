28e9fb4cc98cf7f9a170e1e64a3b858146795f6c
Sajid_ahamed <sajidahamedkhulna2000@gmail.com>
2024-12-04 16:39:55 +0600

    Logic is developed and output are showing showing display button work is pending

--- rps.js
+++ rps.js
@@ -2,22 +2,59 @@ let user_score = 0;
 let computer_score = 0;
 
 const choices = document.querySelectorAll(".choice");
-const genarate_computerchoice = () => {
-    // computer random choice genarating function 
+const msg = document.querySelector("#message");
+
+const show_winner = (user_win) => {
+    if (user_win) {
+        console.log("You win");
+        msg.innerText = "You win";
+    } else {
+        console.log("You lose");
+        msg.innerText=("You lose");
+    }
+};
+
+const draw_game = () => {
+    console.log("Game is draw");
+    msg.innerText=("Game is draw");
+};
+
+const generate_computer_choice = () => {
+    // Computer random choice generating function
     const options = ["rock", "paper", "scissor"];
     const random_index = Math.floor(Math.random() * 3);
     return options[random_index];
-}
-const playgame = (user_choice) => {
-    console.log("User choice :", user_choice);
-    const computer_choice = genarate_computerchoice(); //genarate user choice 
-    console.log("Computer_choice: ",computer_choice);
-}
+};
+
+const play_game = (user_choice) => {
+    console.log("User choice:", user_choice);
+    const computer_choice = generate_computer_choice(); // Generate computer choice
+    console.log("Computer choice:", computer_choice);
+
+    // Draw game conditions:
+    if (computer_choice === user_choice) {
+        draw_game();
+    } else {
+        let user_win = true;
+
+        if (user_choice === "rock") {
+            // Rock vs Paper & Scissor
+            user_win = computer_choice === "paper" ? false : true;
+        } else if (user_choice === "paper") {
+            // Paper vs Rock & Scissor
+            user_win = computer_choice === "rock" ? true : false;
+        } else {
+            // Scissor vs Rock & Paper
+            user_win = computer_choice === "rock" ? false : true;
+        }
+
+        show_winner(user_win);
+    }
+};
+
 choices.forEach(choice => {
-    console.log(choice);
     choice.addEventListener("click", () => {
         const user_choice = choice.getAttribute("id");
-        //   console.log("welcome to the game");
-        playgame(user_choice);
+        play_game(user_choice);
     });
 });
