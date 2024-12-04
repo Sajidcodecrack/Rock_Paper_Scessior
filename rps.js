415b7653da4dfb0f9757ecb494b9781c5aaf69be
Sajid_ahamed <sajidahamedkhulna2000@gmail.com>
2024-12-04 15:49:02 +0600

    Random choice with system and User is integrated.

--- rps.js
+++ rps.js
@@ -4,9 +4,14 @@ let computer_score = 0;
 const choices = document.querySelectorAll(".choice");
 const genarate_computerchoice = () => {
     // computer random choice genarating function 
+    const options = ["rock", "paper", "scissor"];
+    const random_index = Math.floor(Math.random() * 3);
+    return options[random_index];
 }
 const playgame = (user_choice) => {
-    console.log("User choice :", user_choice);  //genarate user choice 
+    console.log("User choice :", user_choice);
+    const computer_choice = genarate_computerchoice(); //genarate user choice 
+    console.log("Computer_choice: ",computer_choice);
 }
 choices.forEach(choice => {
     console.log(choice);
