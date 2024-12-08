29efcb197c674987a4ab893232f69d98721af36f
Sajid_ahamed <sajidahamedkhulna2000@gmail.com>
2024-12-08 21:53:17 +0600

    All the things are included game is ready for user-testing

--- rps.js
+++ rps.js
@@ -3,20 +3,30 @@ let computer_score = 0;
 
 const choices = document.querySelectorAll(".choice");
 const msg = document.querySelector("#message");
+const user_score_count = document.querySelector("#user_score");
+const computer_score_count = document.querySelector("#computer_score");
 
-const show_winner = (user_win) => {
+const show_winner = (user_win, user_choice, computer_choice) => {
     if (user_win) {
+        user_score++;
+        user_score_count.innerText = user_score;
         console.log("You win");
-        msg.innerText = "You win";
+        msg.innerText = `You Win! Your ${user_choice} beats ${computer_choice}`;
+        msg.style.backgroundColor = "green";
     } else {
+        computer_score++;
+        computer_score_count.innerText = computer_score;
+
         console.log("You lose");
-        msg.innerText=("You lose");
+        msg.innerText = `You Lose! Your ${computer_choice} beats ${user_choice}`;
+        msg.style.backgroundColor = "red";
     }
 };
 
-const draw_game = () => {
+const draw_game = (user_choice, computer_choice) => {
     console.log("Game is draw");
-    msg.innerText=("Game is draw");
+    msg.innerText = `Game is drawn! Your ${user_choice} and Computers ${computer_choice} are same! Try again`;
+    msg.style.backgroundColor = "#081b31";
 };
 
 const generate_computer_choice = () => {
@@ -33,7 +43,7 @@ const play_game = (user_choice) => {
 
     // Draw game conditions:
     if (computer_choice === user_choice) {
-        draw_game();
+        draw_game(user_choice, computer_choice);
     } else {
         let user_win = true;
 
@@ -48,7 +58,7 @@ const play_game = (user_choice) => {
             user_win = computer_choice === "rock" ? false : true;
         }
 
-        show_winner(user_win);
+        show_winner(user_win, user_choice, computer_choice);
     }
 };
 
